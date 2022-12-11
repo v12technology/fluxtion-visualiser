d414a72fc7ad5b5256ec3fcec3c285b6926ff27e
greg <gregphiggins@gmai.com>
2022-12-11 10:00:15 +0000

    upodated buuild addedd script to build a package for mac using jpackage associated app with *.graphml files

---
+++ dist/buildinstaller.sh
@@ -0,0 +1,1 @@
+jpackage -n fluxtionVisualiser -i . --main-jar fluxtion-visualiser.jar --file-associations FAgraphml.properties
--- src/main/java/com/fluxtion/visualiser/App.java
+++ src/main/java/com/fluxtion/visualiser/App.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2017 V12 Technology Limited
  *
  * This program is free software: you can redistribute it and/or modify
@@ -16,12 +16,25 @@
  */
 package com.fluxtion.visualiser;
 
+import java.awt.*;
+import java.awt.desktop.OpenFilesEvent;
+import java.awt.desktop.OpenFilesHandler;
+import java.io.File;
+
 /**
  * Entry point for Fluxtion graph visualiser.
- * 
+ *
  * @author Greg Higgins (greg.higgins@V12technology.com)
  */
 public class App {
+    private static File fileToOpen;
+
+    static {
+        try {
+            Desktop.getDesktop().setOpenFileHandler(new FileHandler());
+        } catch (Exception ex) {
+        }
+    }
 
     public static void main(String[] args) {
         try {
@@ -31,10 +44,30 @@ public class App {
                     break;
                 }
             }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                 javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(VisualiserAppFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         VisualiserAppFrame frame = new VisualiserAppFrame();
         frame.display();
+        if (fileToOpen != null) {
+            System.out.println("loading - " + fileToOpen);
+            frame.loadFile(fileToOpen);
+        } else if (args.length > 0) {
+            System.out.println("loading - " + args[0]);
+            frame.loadFile(new File(args[0]));
+        }
+    }
+
+    public static class FileHandler implements OpenFilesHandler {
+
+        @Override
+        public void openFiles(OpenFilesEvent e) {
+            // Handle file open event on Mac
+            System.out.println("Received open files event " + e.toString());
+            if (!e.getFiles().isEmpty()) {
+                fileToOpen = e.getFiles().get(0);
+            }
+        }
     }
 }
